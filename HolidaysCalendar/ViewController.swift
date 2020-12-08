53f6d2c206743f5d23a962eccc9ba52e0df00228
Ilja Patrushev <slowtnr@gmail.com>
2020-12-08 15:25:43 +0200

    Initial Commit

---
+++ HolidaysCalendar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HolidaysCalendar
+//
+//  Created by Ilja Patrushev on 8.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
